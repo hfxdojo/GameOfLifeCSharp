c3ab662c4a75fb7bbcf404e11d81c30b86dd3234
Dmitry Khrustalev <dmitrii.khrustalev@gmail.com>
2021-01-24 17:13:25 -0400

    Extract method Decompose

--- GameOfLife/Positioner.cs
+++ GameOfLife/Positioner.cs
@@ -8,10 +8,7 @@ namespace GameOfLife
     {
         public static string InCenter(string gridString, int windowWidth, int windowHeight)
         {
-            var gridLines = gridString.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-
-            var gridWidth = gridLines[0].Length;
-            var gridHeight = gridLines.Length;
+            var (gridLines, gridWidth, gridHeight) = Decompose(gridString);
 
             int horizontalMarginWidth = GetMarginSize(windowWidth, gridWidth);
             int verticalMarginHeight = GetMarginSize(windowHeight, gridHeight);
@@ -22,9 +19,22 @@ namespace GameOfLife
 
             var horizontalMargin = new string(' ', horizontalMarginWidth);
 
-            var totalSize = windowWidth * windowHeight;
+            return Compose(gridLines,
+                           verticalMargin,
+                           horizontalMargin,
+                           horizontalMargin,
+                           verticalMargin,
+                           totalSize: windowWidth * windowHeight);
+        }
 
-            return Compose(gridLines, verticalMargin, horizontalMargin, horizontalMargin, verticalMargin, totalSize);
+        private static (string[] lines, int width, int height) Decompose(string gridString)
+        {
+            var gridLines = gridString.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var gridWidth = gridLines[0].Length;
+            var gridHeight = gridLines.Length;
+
+            return (gridLines, gridWidth, gridHeight);
         }
 
         private static int GetMarginSize(int windowSize, int gridWidth)
@@ -33,18 +43,21 @@ namespace GameOfLife
             return marginSize < 0 ? 0 : marginSize;
         }
 
-        private static string Compose(string[] gridLines, string topMargin, string leftMargin, string rightMargin, string bottomMargin, int totalSize)
+        private static string Compose(string[] gridLines,
+                                      string topMargin,
+                                      string leftMargin,
+                                      string rightMargin,
+                                      string bottomMargin,
+                                      int totalSize)
         {
             /*
-                topMargin
-             ---------------
-             l  |        | r
-             e  |        | i
-             f  |  grid  | g
-             t  |        | h
-                |        | t
-             ---------------
-               bottomMargin
+                   topMargin
+             ----------------------
+             left  |  grid  | right
+             left  |  grid  | right
+             left  |  grid  | right
+             ----------------------
+                  bottomMargin
              */
             var strBuilder = new StringBuilder(totalSize);
             
